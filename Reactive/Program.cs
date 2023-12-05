53fd860b661a988093beb102792653367cdb4a2a
Stefan Cosma <cosma.steffan11@gmail.com>
2023-12-05 16:29:42 +0200

    Start with lab2

---
+++ Reactive/Program.cs
@@ -0,0 +1,26 @@
+﻿using ActressMas;
+using System.Threading;
+
+namespace Reactive
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            EnvironmentMas env = new EnvironmentMas(0, 100);
+
+            var planetAgent = new PlanetAgent();
+            env.Add(planetAgent, "planet");
+            
+            for (int i = 1; i <= Utils.NoExplorers; i++)
+            {
+                var explorerAgent = new ExplorerAgent();
+                env.Add(explorerAgent, "explorer" + i);
+            }
+
+            Thread.Sleep(500);
+
+            env.Start();
+        }
+    }
+}
